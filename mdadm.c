1f89fc21be38fc31ab865eb33f8c0c0ee2e7b871
Utkarsh Choudhary <ufc5009@psu.edu>
2023-02-24 18:46:57 -0500

    Update mdadm.c

--- mdadm.c
+++ mdadm.c
@@ -4,7 +4,7 @@
 
 #include "mdadm.h"
 #include "jbod.h"
-
+int isMount = -1;
 uint32_t encode_op(int cmd, int disk_num, int block_num)
 {
   uint32_t op = 0;
@@ -14,55 +14,89 @@ uint32_t encode_op(int cmd, int disk_num, int block_num)
   return op;
 }
 int mdadm_mount(void) {
+  if (isMount == 1)
+    return -1;
   uint32_t op = encode_op(JBOD_MOUNT,0,0);
-  printf("operation : %d\n", op);
   int c = jbod_operation(op, NULL);
-  printf("Value of C: %d\n",c);
-  int ret = -1;
-  if(c==0)
-  {
-    ret = 1;
-  }
-  return ret;
+  if (c == -1)
+    return -1;
+  isMount = 1;
+  return 1;
 }
-
-int mdadm_unmount(void) {
+int mdadm_unmount(void)
+{
+  if (isMount == -1)
+    return -1;
   uint32_t op = encode_op(JBOD_UNMOUNT,0,0);
-  int a = jbod_operation(op,NULL);
-  printf("operation : %d\n", op);
-  printf("Value of a: %d\n",a);
-  int d = -1;
-  if(a==0)
-  {
-    d = 1;
-  }
-  if(a!=0)
-  {
-    d = -1;
-  }
-  return d;
+  jbod_operation(op,NULL);
+  isMount = -1;
+  return 1;
 }
-
+int translate_bladdress(int ad)
+{
+    int block_num = (ad%65536)/256;
+    return block_num;
+}
+int translate_daddress(int ad)
+{
+  int disk_num = ad/(65536);
+  return disk_num;
+}
+int seekb(int adr)
+    {
+    uint32_t block = encode_op(JBOD_SEEK_TO_BLOCK,0,adr);
+      return block;
+    }
+int seekd(int adr)
+{
+  uint32_t disk = encode_op(JBOD_SEEK_TO_DISK,adr,0);
+  return disk;
+}
+int minimum(int a, int b)
+{
+    if(a<b)
+    {
+      return a;
+    }
+    else
+    {
+      return b;
+    }
+}
 int mdadm_read(uint32_t addr, uint32_t len, uint8_t *buf) 
 {
-  /*int disk_num;
-  int block_num;
-  int offset;
-  //logic for disk_num ,offset and block_num
-  disk_num = addr/(256*256*16);
-  offset = addr%(256*256*16);
-  block_num = disk_num/(256);
-  int offset_block = disk_num%(256);
-  uint32_t op = JBOD_READ_BLOCK;
-  //translate_address(addr, &disk_num, &block_num &offset);
-  //seek() function write seek to disk and seek to block
-  uint8_t mybuf[256];
-  int b = jbod_operation(op, mybuf);
+  if (isMount == -1)
+    return -1;
+  uint32_t op = 0;
+  if(addr + len > 1024 * 1024)
+  {
+    return -1;
+  }
+  if (len > 1024)
+    return -1;
+  if (buf == NULL && len)
+    return -1;
+int num_read = 0;
+uint8_t mybuf[256];
+while(len>0)
+    {
+      int offset = (addr%256);
+      int disknum = addr / 65536;
+      int blocknum = (addr % 65536) / 256;
+      op = seekd(disknum); 
+      jbod_operation(op, mybuf); 
+      op = seekb(blocknum);
+      jbod_operation(op, mybuf);
+      op = encode_op(JBOD_READ_BLOCK,0,0);
+      jbod_operation(op,mybuf);
+      int num_bytes_to_read_from_block = minimum(len, minimum(256,256-offset));
+      memcpy(buf + num_read, mybuf+offset, num_bytes_to_read_from_block);
+      num_read+=num_bytes_to_read_from_block;
+      len-=num_bytes_to_read_from_block;
+      addr+=num_bytes_to_read_from_block;
+    }
+
+  return num_read;
+  }
   
-  //char buf[256];//use seek to disk takes in disk number
-  //uint32_t op = 0;//then seek to block takes in block number
-  //jbod_operation(op, buf);
-  return len;
-*/
- return -1;
-}
+
